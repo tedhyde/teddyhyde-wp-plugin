3f71a6633df2d276389265e6dd04028960a80dc5
Chris Dawson <xrdawson@gmail.com>
2018-01-12 23:33:54 -0800

    Update description

--- teddyhyde-wp-plugin.php
+++ teddyhyde-wp-plugin.php
@@ -1,9 +1,19 @@
 <?php
-/**
-   * Teddy Hyde Wordpress Plugin
-   * @author xrd
-   * @link http://teddyhyde.com
-*/
+  /**
+     * @package Teddy_Hyde_Wordpress
+     * @version 0.0.3
+   */
+
+  /**
+   Plugin Name: Teddy Hyde Wordpress
+   Plugin URI: https://github.com/tedhyde/teddyhyde-wordpress-plugin
+   Description: Teddy Hyde for Wordpress
+   Author: Chris Dawson
+   Version: 0.0.3
+   Author URI: http://www.teddyhyde.com
+   Text Domain: teddy-hyde-wordpress
+   */
+
 function add_gated_excerpt( $content ) {
   
   $excerpt = preg_replace( '/(.*?)\n\n/', '<div id="excerpt">$1</div>', $content );
