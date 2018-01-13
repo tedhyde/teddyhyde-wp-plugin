3d6cc254bcd455af5e73ea135411176bdc5d4747
Chris Dawson <xrdawson@gmail.com>
2018-01-12 23:27:57 -0800

    Add closing brace

--- teddyhyde-wp-plugin.php
+++ teddyhyde-wp-plugin.php
@@ -1,7 +1,17 @@
-function add_crypto_gated_excerpt( $content ) {
+<?php
+/**
+   * Teddy Hyde Wordpress Plugin
+   * @author xrd
+   * @link http://teddyhyde.com
+*/
+function add_gated_excerpt( $content ) {
+  
   $excerpt = preg_replace( '/(.*?)\n\n/', '<div id="excerpt">$1</div>', $content );
   $full = '<div id="full">' + $content + '</div>';
   return $excerpt + $content;
-} // mdc_the_content
+  
+} 
 
-add_filter( 'the_content', 'add_crypto_gated_excerpt' );
+add_filter( 'the_content', 'add_gated_excerpt' );
+
+?>
