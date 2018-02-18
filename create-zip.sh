8beb927348cf9e934f3b6fc650460b7b4630358d
Chris Dawson <xrdawson@gmail.com>
2018-02-18 11:31:00 -0800

    Add options page

---
+++ create-zip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+zip -r teddyhyde-wordpress-plugin.zip teddyhyde-wp-plugin.php
+echo "Now, upload teddyhyde-wordpress-plugin.zip to your site's plugins"
--- teddyhyde-wp-plugin.php
+++ teddyhyde-wp-plugin.php
@@ -14,6 +14,11 @@
    Text Domain: teddy-hyde-wordpress
    */
 
+   /*
+   Special thanks to Ozh for this code sample: http://planetozh.com/blog/wp-content/uploads/2009/05/ozh-configuration-pluginphp.txt
+   From this blog post: http://planetozh.com/blog/2009/05/handling-plugins-options-in-wordpress-28-with-register_setting/
+   */ 
+
 $popup = <<<EOD
 <div id="wrapper">
   <div id="personal_ad_wrapper">
@@ -48,8 +53,14 @@ $popup = <<<EOD
 
 EOD;
 
-$url = get_site_url();
-$hostname = substr( $url, strlen( "http://" )+1);
+  $url = get_site_url();
+  // If we are using localhost, get that, otherwise assume https.
+  if( 0 == strpos( $url, "http://localhost" ) ) {
+    $hostname = substr( $url, strlen( "http://" ));
+  }
+  else {
+    $hostname = substr( $url, strlen( "https://" ));
+  }
 
 $iframe = <<<EOD
 
@@ -91,50 +102,68 @@ function th_assets_setup() {
     wp_enqueue_style( 'teddyhyde_user_css', 'https://cdn.teddyhyde.com/0.0.1/teddyhyde-user.css' );
 }
 
-/*
-// create custom plugin settings menu
-add_action('admin_menu', 'teddy_hyde_plugin_create_menu');
+add_action('admin_init', 'teddyhyde_configuration_init' );
+add_action('admin_menu', 'teddyhyde_configuration_add_page');
 
-
-function teddy_hyde_plugin_create_menu() {
-	 //create new top-level menu
- 	  add_menu_page('Teddy Hyde Settings', 'Teddy Hyde Settings', 'administrator', __FILE__, 'teddy_hyde_plugin_settings_page' , plugins_url('/images/icon.png', __FILE__) );
-
-	  //call register settings function
- 	 add_action( 'admin_init', 'register_teddy_hyde_plugin_settings' );
+// Init plugin options to white list our options
+function teddyhyde_configuration_init(){
+	register_setting( 'teddyhyde_configuration_options', 'teddyhyde_configuration', 'teddyhyde_configuration_validate' );
 }
 
-
-function register_teddy_hyde_plugin_settings() {
-	  //register our settings
-	  register_setting( 'teddy-hyde-plugin-settings-group', 'advertisement' );
-	  register_setting( 'teddy-hyde-plugin-settings-group', 'thanks' );
+// Add menu page
+function teddyhyde_configuration_add_page() {
+	add_options_page('TeddyHyde iFrame Options', 'TeddyHyde Options', 'manage_options', 'teddyhyde_configuration', 'teddyhyde_configuration_do_page');
 }
 
-function teddy_hyde_plugin_settings_page() {
+// Draw the menu page itself
+function teddyhyde_configuration_do_page() {
 ?>
+
 <div class="wrap">
-<h1>Teddy Hyde Settings</h1>
+  <h2>Ozh's Sample Options</h2>
+  <form method="post" action="options.php">
+    <?php settings_fields('teddyhyde_configuration_options'); ?>
+    <?php $options = get_option('teddyhyde_configuration'); ?>
 
-<form method="post" action="options.php">
-<?php settings_fields( 'teddy-hyde-plugin-settings-group' ); ?>
-<?php do_settings_sections( 'teddy-hyde-plugin-settings-group' ); ?>
-<table class="form-table">
-<tr valign="top">
-<th scope="row">Advertisement</th>
-<td><textarea width="100%" rows="5" name="advertisement" value="<?php echo esc_attr( get_option('advertisement') ); ?>"></textarea></td>
-</tr>
-
-<tr valign="top">
-<th scope="row">Thank You</th>
-<td><textarea type="text" name="thanks" value="<?php echo esc_attr( get_option('thanks') ); ?>"></textarea></td>
-</tr>
-</table>
-
-<?php submit_button(); ?>
-
-</form>
+    <script>
+      console.log( "<?php echo htmlspecialchars( $options['popup'] ? $options['popup'] : $popup ); ?>" );
+    </script>
+    
+    <table class="form-table">
+      <tr valign="top">
+	<th scope="row">Advertising-ish popup when payment is declined</th>
+	<td>
+	  <textarea cols="50" rows="6" name="teddyhyde_configuration[popup]">
+            <?php echo htmlspecialchars( $options['popup'] ? $options['popup'] : $popup ); ?>
+	  </textarea>
+	</td>
+      </tr>
+      <tr valign="top">
+	<th scope="row">iFrame configuration</th>
+	<td>
+	  <textarea cols="50" rows="6" name="teddyhyde_configuration[iframe]">
+	    <?php echo htmlspecialchars( $options['popup'] ? $options['iframe'] : $iframe ); ?>
+	  </textarea>
+	</td>
+      </tr>
+    </table>
+    <p class="submit">
+      <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+    </p>
+  </form>
 </div>
-*/
+
+<?php	
+}
+
+// Sanitize and validate input. Accepts an array, return a sanitized array.
+function teddyhyde_configuration_validate($input) {
+	// Our first value is either 0 or 1
+	// $input['option1'] = ( $input['option1'] == 1 ? 1 : 0 );
+	
+	// Say our second option must be safe text with no HTML tags
+        // $input['sometext'] =  wp_filter_nohtml_kses($input['sometext']);
+	return $input;
+}
 
 ?>
